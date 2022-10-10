23bb3fe2365ecc9ff6429ec187ab97c92062b722
BigFloppa <111kpohk@mail.ru>
2022-10-10 14:11:40 +0300

    hi

---
+++ Task/1vvod/1.6.py
@@ -0,0 +1,5 @@
+a = int(input())
+c = a//3600
+d = a//60
+print (c)
+print (d)
---
+++ Task/1vvod/1.7.py
@@ -0,0 +1,7 @@
+a1 = int(input())*3600
+a2 = int(input())*60
+a3 = int(input())
+b1 = int(input())* 3600
+b2 =  int(input()) *60
+b3 =  int(input())
+print (b1+b2+b3-a1-a2-a3)
