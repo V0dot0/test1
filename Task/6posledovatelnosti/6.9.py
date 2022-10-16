30248cd90161ebbc89a8a90a0bddd5cac41c2b23
BigFloppa <111kpohk@mail.ru>
2022-10-16 14:59:20 +0300

    hi

--- Task/6posledovatelnosti/6.9.py
+++ Task/6posledovatelnosti/6.9.py
@@ -1,7 +1,16 @@
-import math
-string_or_letters = input()
-list_of_letters_1 = list(string_or_letters)
-k = float(len(list_of_letters_1)) / 2
-max = math.ceil(k)
-list_of_letters_2 = list_of_letters_1[max: len(list_of_letters_1)] + list_of_letters_1[0: max]
-print("".join(map(str, list_of_letters_2)))
+
+#lst = input()
+lst = 'Abrakadabra'
+l = len(lst) + 1
+part_1 = lst[0:l // 2]
+part_2 = lst[l // 2:]
+
+if (len(lst)/2) % 2 != 0 :
+    k = round(len(lst)/2)
+    b = lst[k]
+    print(lst)
+    print(part_2  + part_1,"Как в примере")
+    print(b+part_2+part_1,"При нечетном значении символ по середине добавляется к первой строке ")
+else:
+    print(lst)
+    print (part_2+part_1)
