c793e63f574772740513ba8b67becdf54d4fb9a9
BigFloppa <111kpohk@mail.ru>
2022-10-16 17:59:13 +0300

    hi

---
+++ Task/6posledovatelnosti/6.12.py
@@ -0,0 +1,13 @@
+#lst = input()
+lst5 = 'h'
+lst = 'ilumahABRIKOShamHamhamABRICOShtata'
+lst1 = list(lst)
+firstPart = lst[lst1.index("h")+1: -1]
+print(firstPart)
+lastPart = lst[::-1][1: lst1.index("h")]
+lastPart = lastPart[::-1]
+print(lastPart)
+finalPart = firstPart.replace(lastPart," ")
+
+finalPart = finalPart.replace(lst5,"H")
+print(finalPart)
