473f1443b897cec59248f45a4e657025afaf27a4
BigFloppa <111kpohk@mail.ru>
2022-10-23 10:23:20 +0300

    hi

--- Task/7slavar/7.3.py
+++ Task/7slavar/7.3.py
@@ -1,29 +1,13 @@
-slavar = {
-          'joe':5,
-            'daun':6,
-            'joe':2}
-#slavar = {}
-Hidden = []
-#number = int(input(" Введите число: "))
-#while number != 0:
-    #key = input(" Введите президента: ")
-    #value = int(input(" Введите его голоса : "))
-    #slavar[key] = value
-    #Hidden.append(key)
-    #number -= 1
-for name in range(slavar):
-    slavar[name] = slavar.get(name, 0) + 1
-print(slavar)
-#print(Hidden)
+slavar = {}
 
-#for word in phone_book:
- #   result[word] = result.get(word, 0) + 1
- #   result2.append( result.get(word, 0) - 1)
-#print(result2)
+number = int(input(" Введите число: "))
+while number != 0:
+    key = input(" Введите президента: ")
+    value = int(input(" Введите его голоса : "))
+    if key not in slavar :
+        slavar[key] = value
+    else:
+        slavar[key] += value
+    number -= 1
 
-#men = int(input())
-#men_works = {}
-#for _ in range(men):
-#    name = input()
- #   men_works[name] = men_works.get(name,0)+1
-#print(sum((i for i in men_works.values() if i >1)))
+print (slavar)
