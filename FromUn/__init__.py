fe4d8fceb17dbbd94be0352b3667a50282789f9b
BigFloppa <111kpohk@mail.ru>
2022-11-05 15:22:00 +0300

    hi!!! help >.<

--- FromUn/__init__.py
+++
@@ -1,2 +0,0 @@
-from .monty import *
-from .birthdays import *
---
+++ Task/PRACTIC/number_2_final/main2.py
@@ -0,0 +1,5 @@
+import Task.PRACTIC.number_2_final
+
+print(Task.PRACTIC.number_2_final.monty_hall(1000))
+
+print(Task.PRACTIC.number_2_final.findChanse(23, 1000))
