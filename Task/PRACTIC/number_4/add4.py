9f539e38238e0b94ccea29132deaf5bcbe50e0c7
BigFloppa <111kpohk@mail.ru>
2022-11-14 18:58:16 +0300

    hi!!! help >.<

--- Task/PRACTIC/number_4/add4.py
+++ Task/PRACTIC/number_4/add4.py
@@ -1,12 +1,12 @@
-def read_file():
+def get_word_list():
     opened_file = open('data.txt')
     word_list = list(set(opened_file.read().splitlines()))
     return word_list
 
 def save_file(txt, wrd):
-    words = read_file()
+    words = get_word_list()
     words.sort()
-    print(words)
+    #print(words)
     count = len(wrd)
 
     return count, words
--- Task/PRACTIC/number_4/main4.py
+++ Task/PRACTIC/number_4/main4.py
@@ -1,16 +1,20 @@
 from Task.PRACTIC.number_4 import add4 as ad
 
-ad.read_file()
-sv = ad.save_file('data.txt', ad.read_file())
+
+#ad.get_word_list()
+sv = ad.save_file('data.txt', ad.get_word_list())
 count = sv[0]
 words = sv[1]
 words = ' '.join(words)
 words = words.replace(" ", "\n")
-print(count, words)
+#print(count, words)
 
 
 text_to_write = count, words
+# представим что это тот же файл, что бы не перезаписывать старый
 new_text = open("dataAf.txt", mode='w+')
+new_text.write(str("# File content 'data.txt'\n"))
+new_text.write(str("Total unique words: "))
 new_text.write(str(count))
-new_text.write(str("\n============\n"))
+new_text.write(str("\n=========================\n"))
 new_text.write(str(words))
