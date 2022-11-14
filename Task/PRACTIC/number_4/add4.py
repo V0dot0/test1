72124a7aa94777fbeb2603e8f319266fd4eb0a1a
BigFloppa <111kpohk@mail.ru>
2022-11-14 19:12:08 +0300

    hi!!! help >.<

--- Task/PRACTIC/number_4/add4.py
+++ Task/PRACTIC/number_4/add4.py
@@ -1,13 +1,22 @@
-def get_word_list():
-    opened_file = open('data.txt')
-    word_list = list(set(opened_file.read().splitlines()))
-    return word_list
+def read_file(requested_file):
+    content = open(requested_file, "r+")
+    returned_file = list(set(content.read().splitlines()))
+    return returned_file
 
-def save_file(txt, wrd):
-    words = get_word_list()
-    words.sort()
-    #print(words)
-    count = len(wrd)
+def save_file(returned_file, requested_file):
+    content = read_file(requested_file)
+    content.sort()
+    count = len(content)
+    words = content
+    words = ' '.join(words)
+    words = words.replace(" ", "\n")
+    text_to_write = count, words
+    returned_file = open(returned_file, mode='w+')
+    returned_file.write(str("# File content "))
+    returned_file.write(str(returned_file.name))
 
-    return count, words
-
+    returned_file.write(str("\nTotal unique words: "))
+    returned_file.write(str(count))
+    returned_file.write(str("\n===========\n"))
+    returned_file.write(str(words))
+    return returned_file
--- Task/PRACTIC/number_4/main4.py
+++ Task/PRACTIC/number_4/main4.py
@@ -1,20 +1,8 @@
 from Task.PRACTIC.number_4 import add4 as ad
 
 
-#ad.get_word_list()
-sv = ad.save_file('data.txt', ad.get_word_list())
-count = sv[0]
-words = sv[1]
-words = ' '.join(words)
-words = words.replace(" ", "\n")
-#print(count, words)
-
-
-text_to_write = count, words
-# представим что это тот же файл, что бы не перезаписывать старый
-new_text = open("dataAf.txt", mode='w+')
-new_text.write(str("# File content 'data.txt'\n"))
-new_text.write(str("Total unique words: "))
-new_text.write(str(count))
-new_text.write(str("\n=========================\n"))
-new_text.write(str(words))
+requested_file = str(input("Enter the name of the file to read and return data: "))
+returned_file = str(input("Enter the name of the file you want to write the data: "))
+#     dataAf.txt
+print(ad.read_file(requested_file))
+ad.save_file(returned_file, requested_file)
