e74c7d4fd206e3e292b19ee02a981d1c06d92e6a
BigFloppa <111kpohk@mail.ru>
2022-11-21 17:11:36 +0300

    hi!!! help >.<

--- Task/PRACTIC/number_5/Error_processing.py
+++ Task/PRACTIC/number_5/Error_processing.py
@@ -1,27 +1,23 @@
-import File_conversion as fc
+import File_conversion
 
 requested_file = str(input("Enter the name of the file to read and return data: "))
-returned_file = fc.read_file(requested_file)
 
-#if File_conversion.debug_file(requested_file) == 1:
-    #print(File_conversion.debug_file(requested_file))
-    #converted_file = File_conversion.file_conversion(requested_file)
-#print(returned_file, "\n", converted_file)
-try:
-    fc.file_conversion(requested_file)
-except TypeError:
-    print("Неверный тип принимаемых данных")
-except FileNotFoundError:
-    print("Такого файла нету")
-except IndexError:
-    print("Пустой файл")
-except ValueError:
-    print("Неверный тип данных в файле, введите значения типа Int()")
-except:
-    print("Неизвестная ошибка")
+if File_conversion.read_file(requested_file) == "ErrorFileNotFound.txt":
+    print("Ошибка в имени файла")
 else:
-    print(fc.file_conversion(requested_file))
-finally:
-    print("======================================")
+    try:
+        print(File_conversion.file_conversion(requested_file))
+    except TypeError:
+        print("Неверный тип принимаемых данных")
+    except IndexError:
+        print("Пустой файл")
+    except ValueError:
+        print("Неверный тип данных в файле, введите значения типа Int()")
+    except FileNotFoundError:
+        print("Такого файла нету")
+    except:
+        print("Неизвестная ошибка")
+    finally:
+       print("===========")
 
-
+    returned_file = File_conversion.read_file(requested_file)
--- Task/PRACTIC/number_5/File_conversion.py
+++ Task/PRACTIC/number_5/File_conversion.py
@@ -1,9 +1,19 @@
+
 def read_file(requested_file):
+    try: content = open(requested_file, "r+")
+    except:
+        requested_file = "ErrorFileNotFound.txt"
+        return requested_file
     content = open(requested_file, "r+")
     returned_file = content.read().splitlines()
     return returned_file
 
 def file_conversion(requested_file):
+    #try: returned_file = read_file(requested_file)
+    #except:
+    #    print("Такого файла нету")
+    #finally:
+    #    print("hi")
     returned_file = read_file(requested_file)
     returned_file = list(map(int, returned_file))
     save = returned_file[0]
@@ -11,6 +21,5 @@ def file_conversion(requested_file):
     if save == len(returned_file):
         return returned_file
     else:
-        print("Количество чисел не соответствует первому значению")
-
-
+        broken = "Количество чисел не соответствует первому значению!"
+        return broken
