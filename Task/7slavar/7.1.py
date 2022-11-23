87e80d16d7ae954ef3b9a85d8dfe330ce68e5568
BigFloppa <111kpohk@mail.ru>
2022-11-23 11:54:55 +0300

    hi!!! help >.<

--- Task/7slavar/7.1.py
+++ Task/7slavar/7.1.py
@@ -1,9 +1,10 @@
+# one two one two three two four three
+
 sentence = input()
-#sentence = 'ham tatata tratata help tam param param tam tam'
 words = sentence.split(' ')
 lst = []
 result = {}
 for word in words:
     result[word] = result.get(word, 0) + 1
-    lst.append( result.get(word, 0) - 1)
-print(lst)
+    lst.append(result.get(word, 0) - 1)
+print(str(lst).replace("[", "").replace("]", "").replace(",", ""))
--- Task/7slavar/7.2.py
+++ Task/7slavar/7.2.py
@@ -1,14 +1,26 @@
+# 3
+# Hello Hi
+# Bye Goodbye
+# List Array
+# Goodbye
+key = []
 slavar = {}
+keySpecial = "None"
 number = int(input(" Введите число: "))
 while number != 0:
-    key = input(" Введите слово: ")
-    value = input(" Введите его синоним: ")
-    slavar[key] = value
-    if number-1 == 0 :
-        keySpecial = input(" Введите финальное слово: ")
+    inpud = input(" Введите слово и синоним: ")
+    key = inpud[0]
+    value = inpud[1]
+    print(key, value)
+    # key = input(" Введите слово: ")
+    # value = input(" Введите его синоним: ")
+
+    # slavar[value] = key
+    if number - 1 == 0:
+        keySpecial = input(" Введите финальный синоним: ")
         break
     number -= 1
 
-#print (phone_book)
-#print (keySpecial)
-print (slavar.get(keySpecial))
+# print (phone_book)
+# print (keySpecial)
+print(slavar.get(keySpecial))
