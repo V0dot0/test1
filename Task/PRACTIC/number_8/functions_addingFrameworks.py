c3345f90e1abd7a1a711f873ce0a7453ba946224
BigFloppa <111kpohk@mail.ru>
2022-12-12 12:37:05 +0300

    fix

--- Task/PRACTIC/number_8/functions_addingFrameworks.py
+++ Task/PRACTIC/number_8/functions_addingFrameworks.py
@@ -1,8 +1,13 @@
-def hi():
+import pymorphy2
+from translate import Translator
+
+
+translator = Translator(from_lang="ru", to_lang="en")
+morph = pymorphy2.MorphAnalyzer()
+
+def unsorted_list_creation():
     lstx = []
     txt_open: io.TextIOWrapper = open("input.txt", mode='r', encoding='utf-8')
-    key = "hi hello \n dum"#text here full line#input()
-    key = txt_open
     bad_chars = [';','!', "*","?","1","2","3","4","5","6","7","8","9","0",",",":",".","-"]
     for word in txt_open:
         word = word.replace("\n", "")
@@ -10,7 +15,6 @@ def hi():
             word = word.replace(i, '')
         temp_word = word.split(' ')
         lstx.append(temp_word)
-    #print(lstx)
     ####
 
     lstx2 = []
@@ -18,37 +22,33 @@ def hi():
         for words in i:
             if len(words)!=0:
                 lstx2.append(words.lower())
-    #print(lstx2)
 
-    lst = []
     lst2 = {}
     for i in lstx2:
-        #print(i)
         if i in lst2:
             lst2[i] += 1
         else:
             lst2[i] = 1
-    print("base ",lst2)
-    ###
+    return lst2
 
+
+def sorting_list(requested_list):
+    print("Начался перевод, по завершении программы будет изменен output.txt")
+    lst2 = requested_list
     lstD = dict(sorted(lst2.items()))
-    print("dict ",lstD)
-
-
-    lst3 = sorted(lstD, key=lstD.get, reverse=True) #list sorts by most amount
-    #print(lst3)
-    lst3.sort(key=lambda x: x[0])
-    #print(lst3)
+    lst3 = sorted(lstD, key=lstD.get, reverse=True)
 
     txt_write = open("output.txt", mode='w+', encoding='utf-8')
     for sord in lst3:
         for key, value in lstD.items():
-            if sord in key:
-                 #print(sord, value)
-                 txt_write.write(sord)
-                 txt_write.write(" ")
+            if sord == key:
+                 morph1 = morph.parse(sord)[0]
+                 sord = morph1.normal_form
+                 sord_translated = translator.translate(sord)
+                 txt_write.write(str(sord))
+                 txt_write.write("|")
+                 txt_write.write(str(sord_translated))
+                 txt_write.write("|")
                  txt_write.write(str(value))
                  txt_write.write('\n')
 
-
-hi()
--- Task/PRACTIC/number_8/main_addingFrameworks.py
+++ Task/PRACTIC/number_8/main_addingFrameworks.py
@@ -1,4 +1,4 @@
-import pymorphy2
-morph = pymorphy2.MorphAnalyzer()
-p = morph.parse('стали')[0]
-print(p.normalized)
+import functions_addingFrameworks as fun
+
+list_1 = fun.unsorted_list_creation()
+fun.sorting_list(list_1)
