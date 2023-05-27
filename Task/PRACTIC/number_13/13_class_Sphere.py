9c2642e746376fe39be354609fdc0b02afcb7aea
IlyaIlyaIlyaIlya <111kpohk@mail.ru>
2023-05-27 14:52:14 +0300

    sphere

--- Task/PRACTIC/number_13/13_class_Sphere.py
+++ Task/PRACTIC/number_13/13_class_Sphere.py
@@ -8,16 +8,40 @@ class Sphere:
         self.y = y
         self.z = z
 
-    def getVolume(self):
+    def get_volume(self):
         self.volume = (4 / 3) * math.pi * math.pow(self.radius, 3)
         return self.volume
 
-    def getSquare(self):
+    def get_square(self):
         self.square = math.pi * 4 * math.pow(self.radius, 2)
 
-    def getRadius(self):
+    def get_radius(self):
         return self.radius
 
-    def getCentre(self):
-        center = 0
+    def get_center(self):
+        self.center = (self.x, self.y, self.z)
         return self.center
+
+    def set_radius(self, r):
+        self.radius = r
+
+    def set_center(self, x0, y0, z0):
+        self.x = x0
+        self.y = y0
+        self.z = z0
+
+    def is_point_inside_xyz(self, x0, y0, z0):
+        if (math.pow((self.x - x0), 2) + math.pow((self.y - y0), 2)
+                + math.pow((self.z - z0), 2) < (math.pow(self.radius, 2))):
+            return True
+        else:
+            return False
+
+print("  Tests:")
+s0 = Sphere(0.5) # test sphere creation with radius and default center
+print(s0.get_center()) # (0.0, 0.0, 0.0)
+print(s0.get_volume()) # 0.523598775598
+print(s0.is_point_inside_xyz(0, -1.5, 0)) # False
+s0.set_radius(1.6)
+print(s0.is_point_inside_xyz(0, -1.5, 0)) # True
+print(s0.get_radius()) # 1.6
