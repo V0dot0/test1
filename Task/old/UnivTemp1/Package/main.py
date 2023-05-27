2b0b9ba804cf228878b3fc2151206fa3dae3bbe2
IlyaIlyaIlyaIlya <111kpohk@mail.ru>
2023-05-27 11:08:46 +0300

    rename1

--- Task/old/UnivTemp1/Package/main.py
+++ Task/old/UnivTemp1/Package/main.py
@@ -2,5 +2,5 @@ import Geometry
 
 print(dir(Geometry)) # Выведем список допустимых имён (атрибутов) в алфавитном порядке для объекта Geometry
 
-print(Geometry.sq_pr(2,5))
-print(Geometry.get_perimetr(2,1,2,1))
+print(Geometry.sq_pr(2, 5))
+print(Geometry.get_perimetr(2, 1, 2, 1))
