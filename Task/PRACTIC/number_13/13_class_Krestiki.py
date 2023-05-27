86cdf84991494eeec0bdcd9ba228086467a7b9bf
IlyaIlyaIlyaIlya <111kpohk@mail.ru>
2023-05-27 16:16:17 +0300

    krestiki and more

---
+++ Task/PRACTIC/number_13/13_class_Krestiki.py
@@ -0,0 +1,176 @@
+
+import math
+import random
+
+bord = {'7': ' ', '8': ' ', '9': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '1': ' ', '2': ' ', '3': ' '}
+
+bord_keys = []
+for key in bord:
+    bord_keys.append(key)
+
+def printBord(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def game():
+    turn = 'X'
+    count = 0
+
+    for i in range(10):
+
+        if count == 9:
+            print("\nКонец\n")
+            print("Ничья")
+
+        printBord(bord)
+        print("Ваш шаг," + turn + ".Выберите место\n"
+                                  "7-8-9\n"
+                                  "4-5-6\n"
+                                  "1-2-3\n ")
+
+        move = input()
+        print("\n")
+
+        if bord[move] == ' ':
+            bord[move] = turn
+            count += 1
+
+        else:
+            print("Занято.\n Выберите другое место")
+            continue
+
+        if bord['7'] == bord['8'] == bord['9'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['4'] == bord['5'] == bord['6'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['1'] == bord['2'] == bord['3'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['1'] == bord['4'] == bord['7'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['2'] == bord['5'] == bord['8'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['3'] == bord['6'] == bord['9'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['7'] == bord['5'] == bord['3'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['1'] == bord['5'] == bord['9'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+
+# копия с примитывным ии
+def gameAI():
+    turn = 'X'
+    count = 0
+
+    for i in range(10):
+
+        if count == 9:
+            print("\nКонец\n")
+            print("Ничья")
+
+        printBord(bord)
+        print("Ваш шаг," + turn + ".Выберите место\n"
+                                  "7-8-9\n"
+                                  "4-5-6\n"
+                                  "1-2-3\n ")
+        if turn == 'X':
+            move = input()
+        else:
+            move = str(random.randint(1, 9))
+        print("\n")
+
+        if bord[move] == ' ':
+            bord[move] = turn
+            count += 1
+
+        else:
+            print("Занято.\n Выберите другое место")
+            continue
+
+        if bord['7'] == bord['8'] == bord['9'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['4'] == bord['5'] == bord['6'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['1'] == bord['2'] == bord['3'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['1'] == bord['4'] == bord['7'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['2'] == bord['5'] == bord['8'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['3'] == bord['6'] == bord['9'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['7'] == bord['5'] == bord['3'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+        elif bord['1'] == bord['5'] == bord['9'] != ' ':
+            printBord(bord)
+            print("\nКонец\n")
+            print(" **** " + turn + " победил. ****")
+            break
+
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+
+
+
+useAi = input("Используем случайный ии? 1 = да, 2 = нет.")
+if (useAi == "1" or useAi == "да"):
+    gameAI()
+else:
+    game()
+
--- Task/PRACTIC/number_13/13_class_SuperStr.py
+++ Task/PRACTIC/number_13/13_class_SuperStr.py
@@ -1,1 +1,32 @@
-13_class_Sphere.py
+
+import math
+
+class SuperStr(str):
+    def is_repeatance(self, s0):
+        if (type(s0) == str):
+            if (len(self) % len(s0) == 0):
+                return True
+            else:
+                return False
+        else:
+            return False
+    def is_palindrom(self):
+        if (self == ''.join(reversed(self))):
+            return True
+        else:
+            return False
+
+
+# Тесты
+s = SuperStr("123123123123")
+print(s.is_repeatance("123")) # True
+print(s.is_repeatance("123123")) # True
+print(s.is_repeatance("123123123123")) # True
+print(s.is_repeatance("12312")) # False
+print(s.is_repeatance(123)) # False
+print(s.is_palindrom()) # False
+print(s) # 123123123123 (строка)
+print(int(s)) # 123123123123 (целое число)
+print(s + "qwe") # 123123123123qwe
+p = SuperStr("123_321")
+print(p.is_palindrom()) # True
